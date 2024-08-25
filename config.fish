f47ae397557238819a40dff66b772cb0cca65ffd
Ashikur Rahman Shad <97606195+ashikur-rahman-shad@users.noreply.github.com>
2024-08-25 17:15:38 +0600

    config example file to implement the theme

---
+++ config.fish
@@ -0,0 +1,6 @@
+set -g kawaii_fish_dir (realpath ~/.config/fish/kawaii-fish)
+source "$kawaii_fish_dir/kawaii.fish"
+
+set -x ANDROID_HOME ~/Android/Sdk
+
+fish_add_path -g $ANDROID_HOME/cmdline-tools/latest/bin $ANDROID_HOME/platform-tools $ANDROID_HOME/emulator
